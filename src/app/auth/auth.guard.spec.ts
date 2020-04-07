5cb671bedad4872c0dabd45f2136a3d4b5e5bef0
mike <m@d>
2020-04-06 21:53:54 -0400

    add test cases to authguard

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,19 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
-import { RouterTestingModule } from '@angular/router/testing';
+import { RouterTestingModule, setupTestingRouter } from '@angular/router/testing';
+import { AuthService } from './auth.service';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
+  let authServiceStub: Partial<AuthService>;
+
   beforeEach(() => {
+    authServiceStub = {
+      isLoggedIn: false
+    };
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: authServiceStub }]
     });
     guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+  it('should return false when not logged in', () => {
+    expect(guard.checkLogin('fakeUrl')).toEqual(false);
+  });
+  it('should not load when not logged in', () => {
+    expect(guard.canLoad(setupTestingRouter.prototype)).toEqual(false);
+  });
+  it('should return true when logged in', () => {
+    authServiceStub.isLoggedIn = true;
+    expect(guard.checkLogin('fakeUrl')).toEqual(true);
+  });
+  it('should load when logged in', () => {
+    authServiceStub.isLoggedIn = true;
+    expect(guard.canLoad(setupTestingRouter.prototype)).toEqual(true);
   });
 });
